9db764ac001a15b1043f797e6a01223bb243a690
stenders <stenders@163.com>
2013-11-29 00:07:14 +0800

    backbone learning

--- todo/main.js
+++ todo/main.js
@@ -1,28 +1,75 @@
+(function(){
 
-var Person = Backbone.Model.extend({
+var app = {
+  Models: {},
+  Views : {},
+  Collections: {}
+}
+var template = function(ele){
+  return _.template($('#' + ele).html())
+}
+
+app.Models.Person = Backbone.Model.extend({
   defaults: {
-    name : 'Doe',
-    age: 28,
-    occupation: 'worker'
+    name : 'John Doe',
+    age  : 20,
+    occupation: 'web developer'
   }
 })
 
-
-var PersonView = Backbone.View.extend({
-  tagName: 'li',
-  
-  template: _.template($('#personTemp').html()),
-
+app.Views.Person = Backbone.View.extend({
+  tagName : 'li',
+  template: template('personTemp'),
   initialize: function(){
     this.render()
   },
-
   render: function(){
-    this.$el.html(this.template(this.model.toJSON()))
+    this.$el.html( this.template(this.model.toJSON()) )
+    return this
   }
 })
 
-var person = new Person
-var personView = new PersonView({
-  model : person
+app.Views.People = Backbone.View.extend({
+  tagName : 'ul',
+  initialize: function(){
+    this.render()
+  },
+  render: function(){
+    this.collection.each(function(person){
+      var view = new app.Views.Person({model: person})
+      this.$el.append(view.el)
+    }, this)
+    return this
+  }
 })
+
+app.Collections.People = Backbone.Collection.extend({
+  model: app.Models.Person
+})
+
+var peopleCollection = new app.Collections.People([
+  {
+    name : 'luck',
+    age  : 19
+  },
+  {
+    name : 'Lucy',
+    age  : 20,
+    occupation: 'graphic designer'
+  },
+  {
+    name : 'lily',
+    age  : 21,
+    occupation: 'web designer'
+  }
+])
+
+var peopleView = new app.Views.People({
+  collection: peopleCollection
+})
+
+$(document).ready(function(){
+  $(document.body).append(peopleView.el)
+})
+
+}())
